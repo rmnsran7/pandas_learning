41f9055ea157b014d3234e7896ecaed9eea2004e
= <=>
2023-05-17 23:24:52 -0500

    This is the file that merges multiple csv's. But it takes 1st line from 1st file, then 1st line from second file.. and so on. But I want csv one after other. In the next version I will try to update that

--- list_zip_learning.py
+++ list_zip_learning.py
@@ -1,14 +1,40 @@
-import random
+import csv
+import os
 
-nouns = ["dog", "cat", "house", "car", "book"]
-verbs = ["run", "eat", "cry", "read", "sleep"]
-adjs = ["happy", "sad", "big", "small", "brave", "funny"]
-i = 0
-while i < 30:
-    zipped = list(zip(adjs, nouns, verbs))
-    random.shuffle(zipped)
-    random_sentence = zipped[0]
+def merge_csv_files(input_files, output_file):
+    csv_files = []
+    readers = []
 
-    sentence = " ".join(random_sentence)
-    print(sentence)
-    i += 1
+    try:
+        # Open all input CSV files
+        for file in input_files:
+            if os.path.isfile(file):
+                csv_files.append(open(file, 'r'))
+                readers.append(csv.reader(csv_files[-1]))
+            else:
+                raise FileNotFoundError(f"File '{file}' not found.")
+
+        # Create a writer for the output file
+        output_file_obj = open(output_file, 'w', newline='')
+        output_writer = csv.writer(output_file_obj)
+
+        try:
+            # Iterate over rows in the CSV files
+            for rows in list(zip(*readers)):
+                # Merge corresponding columns from each row
+                for line in rows:
+                    output_writer.writerow(line)
+        finally:
+            # Close the output file
+            output_file_obj.close()
+
+    finally:
+        # Close all input files
+        for file in csv_files:
+            file.close()
+
+
+
+csv_file = "datasets/choclate protfolio project - 11.csv"
+output_file = "datasets/merged_file.csv"
+merge_csv_files([csv_file, csv_file], output_file)
